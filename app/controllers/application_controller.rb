ea8a407b2ba418a06da47f9e6c29bb7e0fa5e8b1
Xavier Canal i Masjuan <xeviknal@gmail.com>
2015-05-22 23:36:35 +0200

    refs #012 Redirect to lists when user signed up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :redirect_to_list, if: :user_logged?, if: :root_path?
+
+  def user_logged?
+    current_user.present?
+  end
+
+  def root_path?
+    request.path == "/"
+  end
+
+  def redirect_to_list
+    redirect_to lists_path
+  end
 end
